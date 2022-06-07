b72a0b56d155d44f3c8514056490ae0ac1a36e0e
Zeta279 <equi27@live.com.ar>
2022-06-07 14:10:04 -0300

    Archivos listados

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TrabajoPractico8{
+    class Program{
+        static void Main(string[] args){
+            string path;
+            List<string> archivos = new List<string>();
+
+            Console.WriteLine("Ingrese una ruta: ");
+            path = Console.ReadLine();
+
+            if(Directory.Exists(path)){
+                archivos = Directory.GetFiles(path).ToList();
+
+                foreach(string s in archivos){
+                    Console.WriteLine(s);
+                }
+            }
+            else{
+                Console.WriteLine("La ruta ingresada no existe");
+            }
+
+            
+        }
+    }
+}
